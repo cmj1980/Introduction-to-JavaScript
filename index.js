18361f455ebcc2742a1e2df9f62bd55070a0e3c2
charles johnson <dagoclmt@gmail.com>
2022-03-09 16:48:10 -0800

    Java Intro resub

--- index.js
+++ index.js
@@ -145,8 +145,8 @@ NOTE 2: This is a great time to check the tests to see what it expects, versus w
         So, on this one test, the weight would be 4 pounds, and the age would be 1 years old. It's expecting your function to return a decimal number of 0.2
 */  
 
-function hungryDog(age, weight){
-  if(age >= 1 && weight <=5){
+function hungryDog(weight, age){
+  if(age >= 1 && weight <=5) {
     return weight * 0.05
   }else if(age >= 1 && weight >= 6 && weight <= 10 ){
     return weight * 0.04
@@ -154,18 +154,19 @@ function hungryDog(age, weight){
     return weight * 0.03
   }else if(age >= 1 && weight >= 15){
     return weight * 0.02
-  }else if(age < 0.333){
+  }else if(age < 0.33){
     return weight * 0.10
-  }else if(age <0.583 && age >= 0.333  ){
+  }else if(age <0.58 && age >= 0.33  ){
     return weight * 0.05
-  }else if(age < 1 && age >= 0.583 ){
+  }else if(age < 1 && age >= 0.58 ){
     return weight * 0.04
   } else {
     return 'Please enter valid weight'
   }
   
 }
-console.log(hungryDog(1, 15))
+console.log(hungryDog(15, 1))
+
 
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
@@ -209,7 +210,7 @@ let computer =  Math.floor(Math.random()*3)
   } else if(user === "scissors" && computer === "paper"){
     return `you win!`;
   }else{
-    return 'you lose'
+    return 'you lose!'
   }
 
 }
